ba73a75325fbf180b48fa5095169ff36d4f9c1f6
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-05-13 17:22:41 +0200

    Component Exercise #1

--- main.js
+++ main.js
@@ -1,41 +1,27 @@
-Vue.component('tasks', {
-    props: ['list'],
+Vue.component('alert', {
+    template: '#alert-template',
 
-    template: '#tasks-template',
-
-    computed: {
-        remaining: function () {
-            return this.list.filter(this.inProgress).length;
-        },
+    props: ['type'],
+    
+    data: function () {
+        return {
+            show: true
+        };
     },
 
-    methods: {
-        isCompleted: function (task) {
-            return task.completed;
-        },
+    computed: {
+        alertClasses: function () {
+            var type = this.type;
 
-        inProgress: function (task) {
-            return ! task.completed;
+            return {
+                'Alert': true,
+                'Alert--success': type == 'success',
+                'Alert--error': type == 'error',
+            }
         },
-
-        deleteTask: function (task) {
-            this.list.$remove(task);
-        },
-
-        clearCompleted: function () {
-            this.list = this.list.filter(this.inProgress);
-        }
     },
 });
 
 var vm = new Vue({
     el: '#app',
-
-    data: {
-        tasks: [
-            { body: 'Go to the store', completed: false },
-            { body: 'Go to the bank', completed: false },
-            { body: 'Go to the doctor', completed: true },
-        ],
-    },
 });
