af9818dce03c7247a0932ac04dfe1d076bf0766b
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-05-13 20:51:00 +0200

    Vue and Laravel Workflow 1

---
+++ laravel/resources/assets/js/components/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :class="alertClasses" v-show="show">
+        <slot></slot>
+        <span class="Alert__close" @click="show = false">x</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['type'],
+
+        data() {
+            return {
+                show: true
+            };
+        },
+
+        computed: {
+            alertClasses() {
+                var type = this.type;
+
+                return {
+                    'Alert': true,
+                    'Alert--success': type == 'success',
+                    'Alert--error': type == 'error',
+                }
+            },
+        },
+    };
+</script>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -2,27 +2,28 @@
 <html>
     <head>
         <title>Laravel</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ url('css/app.css') }}">
     </head>
     <body>
-        <div class="container">
-            <tasks></tasks>
+        <div id="app" class="container">
+
+            <alert type="info">
+                <strong>Info!</strong>
+                Your account has ben updated.
+            </alert>
+
+            <alert type="success">
+                <strong>Success!</strong>
+                Your account has ben updated.
+            </alert>
+
+            <alert type="error">
+                <strong>Error!</strong>
+                Your account has not ben updated.
+            </alert>
+
         </div>
-        
-        <template id="tasks-template">
-            <h1>My Tasks</h1>
-            
-            <ul class="list-group">
-                <li class="list-group-item" v-for="task in list">
-                    @{{ task.body }}
 
-                    <strong @click="deleteTask(task)">X</strong>
-                </li>
-            </ul>
-        </template>
-
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.24/vue.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
-        <script src="/js/main.js"></script>
+        <script src="{{ url('js/main.js') }}"></script>
     </body>
 </html>
