db7d2b1fa2acea5b1d79f3d04f2b6d898ff99244
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-05-15 11:07:42 +0200

    Vue Filter Essentials

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-Vue.component('alert', {
-    template: '#alert-template',
+Vue.filter('jsonIt', function (value) {
+    return JSON.stringify(value);
+});
 
-    props: ['type'],
-    
-    data: function () {
-        return {
-            show: true
-        };
-    },
-
-    computed: {
-        alertClasses: function () {
-            var type = this.type;
-
-            return {
-                'Alert': true,
-                'Alert--success': type == 'success',
-                'Alert--error': type == 'error',
-            }
-        },
-    },
+Vue.filter('role', function (value, role) {
+    return value.filter(function (item) {
+        return item.role == role;
+    });
 });
 
 var vm = new Vue({
     el: '#app',
+    
+    data: {
+        message: 'hello world',
+
+        people: [
+            { name: 'Joe', role: 'admin' },
+            { name: 'Susan', role: 'admin' },
+            { name: 'Frank', role: 'student' },
+            { name: 'Jeffrey', role: 'admin' },
+            { name: 'Wladimir', role: 'student' },
+            { name: 'Peter', role: 'student' },
+        ]
+    },
 });
