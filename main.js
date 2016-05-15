a4ba79fd3917fc67193dca27052bd6262f8d1319
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-05-15 18:15:45 +0200

    Vue Transitions and Animations 1

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
-Vue.component('message', {
-    template: '<input v-model="message" @keyup.enter="storeMessage">',
-
-    data: function () {
-        return {
-            message: '',
-        };
-    },
-
-    methods: {
-        storeMessage: function () {
-            // console.log('Storing: ' + this.message);
-
-            this.$dispatch('new-message', this.message);
-
-            this.message = '';
-        },
-    },
-});
-
 var vm = new Vue({
     el: '#app',
     
     data: {
-        messages: [],
-    },
-    
-    methods: {
-        handleNewMessage: function (message) {
-            this.messages.push(message);
-
-            console.log('Parent is handling the ' + message);
-        }
+        show: true,
     },
 });
